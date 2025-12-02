026d2d0872b4b1f96a8de0aa59272907ed58c627
Aravind <gmarav005@gmail.com>
2025-12-02 23:24:54 +0530

    Add key

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import Die from "./Die"
 import { useState } from "react"
+import Die from "./Die.jsx"
+import { nanoid } from "nanoid"
 
 export default function App() {
-    
     const [dice, setDice] = useState(generateAllNewDice())
-    
+
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
-            .map(() => Math.ceil(Math.random() * 6))
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
     }
-    
+
     function rollDice() {
         setDice(generateAllNewDice())
-        
     }
-    
-    const diceElements = dice.map(num => <Die value={num} />)
-    
+
+    const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value} />)
+
     return (
         <main>
             <div className="dice-container">
                 {diceElements}
             </div>
-            
-            {<button onClick={rollDice}>Roll Dice</button>}
-            
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     )
 }
