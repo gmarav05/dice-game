eded16b1a71845464af38c4b90137ee0177ba69b
Aravind <gmarav005@gmail.com>
2025-12-03 09:57:42 +0530

    Add Held condition

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,11 @@ export default function App() {
     }
 
     function hold(id) {
-        console.log(id)
+        setDice(oldDice => oldDice.map(die =>
+            die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
+        ))
     }
 
     const diceElements = dice.map(dieObj => (
