2fa5e188422deff764a6177be5857c31983c7c43
Aravind <gmarav005@gmail.com>
2025-12-03 14:55:57 +0530

    Add Readme

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ export default function App() {
             <div aria-live="polite" className="sr-only">
                 {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
             </div>
-            <h1 className="title">Tenzies</h1>
+            <h1 className="title">Dice Game</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dice-game/', // <-- Add this line
+  base: '/dice-game/', 
 })
