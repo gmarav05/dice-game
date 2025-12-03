c54ec3d2ecf25dcefd842c8ea9342ea43a2cc30e
Aravind <gmarav005@gmail.com>
2025-12-03 12:58:19 +0530

    Add NewGame btn func

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
 
 export default function App() {
     const [dice, setDice] = useState(generateAllNewDice())
 
     const gameWon = dice.every(die => die.isHeld) &&
-        dice.every(die => die.value === dice[0].value)
-
+        dice.every(die => die.value === dice[0].value) 
+    const { width, height } = useWindowSize()
+        
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
@@ -45,6 +48,10 @@ export default function App() {
 
     return (
         <main>
+            {gameWon && <Confetti
+                width={width}
+                height={height}
+             />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
